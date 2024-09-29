82860bfe94bf2bb34cb4488cb5192b7f94b8a391
Muhammad Shabbir <110288732+muhammadshabbir07@users.noreply.github.com>
2024-09-29 22:19:00 +0500

    chapter 9 to 11 complete

--- JavaScript/JavaScript Beginners/Assingments/Chapter-9-11/main.js
+++ JavaScript/JavaScript Beginners/Assingments/Chapter-9-11/main.js
@@ -1,161 +1,236 @@
-// Q1, Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this:
-// “Welcome to city of lights”
+// // Q1, Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this:
+// // “Welcome to city of lights”
 
-var userInput = prompt("Enter your city name");
+// var userInput = prompt("Enter your city name");
 
-if (userInput.toLowerCase() == "karachi") {
-  console.log("Welcome to city of lights");
-}
+// if (userInput.toLowerCase() == "karachi") {
+//   console.log("Welcome to city of lights");
+// }
 
-// Q2, Write a program to take “gender” as input from user. If the user is male, give the message:
-// Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+// // Q2, Write a program to take “gender” as input from user. If the user is male, give the message:
+// // Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
 
-var genInput = prompt("Enter your gender");
+// var genInput = prompt("Enter your gender");
 
-if (genInput.toLowerCase() == "male") {
-  console.log("Good Morning Sir");
-} else if (genInput.toLowerCase() == "female") {
-  console.log("Good Morning Mam");
-} else {
-  console.log("Get Lost...");
-}
+// if (genInput.toLowerCase() == "male") {
+//   console.log("Good Morning Sir");
+// } else if (genInput.toLowerCase() == "female") {
+//   console.log("Good Morning Mam");
+// } else {
+//   console.log("Get Lost...");
+// }
 
-// Q3, Write a program to take input color of road traffic signal from the user & show the message according
-//  to this table:
+// // Q3, Write a program to take input color of road traffic signal from the user & show the message according
+// //  to this table:
 
-var signalColorInput = prompt("Enter a road trafic signal color name");
+// var signalColorInput = prompt("Enter a road trafic signal color name");
 
-if (signalColorInput.toLowerCase() == "red") {
-  console.log("Must Stop");
-} else if (signalColorInput.toLowerCase() == "yellow") {
-  console.log("Ready to move");
-} else if (signalColorInput.toLowerCase() == "green") {
-  console.log("Move now");
-}
+// if (signalColorInput.toLowerCase() == "red") {
+//   console.log("Must Stop");
+// } else if (signalColorInput.toLowerCase() == "yellow") {
+//   console.log("Ready to move");
+// } else if (signalColorInput.toLowerCase() == "green") {
+//   console.log("Move now");
+// }
 
-// Q4, Write a program to take input remaining fuel in car (inlitres) from user. If the current fuel is less
-// than 0.25litres, show the message “Please refill the fuel in your car”
+// // Q4, Write a program to take input remaining fuel in car (inlitres) from user. If the current fuel is less
+// // than 0.25litres, show the message “Please refill the fuel in your car”
 
-var remainingFuel = Number(
-  prompt("Enter a remaining fuel in car \nHint: (inlitres)")
-);
+// var remainingFuel = Number(
+//   prompt("Enter a remaining fuel in car \nHint: (inlitres)")
+// );
 
-if (remainingFuel < 0.25) {
-  console.log("Please refill the fuel in your car");
-} else {
-  console.log("your fuel is good");
-}
+// if (remainingFuel < 0.25) {
+//   console.log("Please refill the fuel in your car");
+// } else {
+//   console.log("your fuel is good");
+// }
 
-// Q5, Run this script, & check whether alert message would be displayed or not. Record the outputs.
-//a.
-var a = 4;
-if (++a === 5) {
-  alert("given condition for variable a is true");
-}
+// // Q5, Run this script, & check whether alert message would be displayed or not. Record the outputs.
+// //a.
+// var a = 4;
+// if (++a === 5) {
+//   alert("given condition for variable a is true");
+// }
 
-// Answer: alert message displayed
+// // Answer: alert message displayed
 
-// b.
-var b = 82;
-if (b++ === 83){
-    alert("given condition for variable b is true");
-}
+// // b.
+// var b = 82;
+// if (b++ === 83){
+//     alert("given condition for variable b is true");
+// }
 
-// Answer: alert message not displayed
+// // Answer: alert message not displayed
 
-// c.
-var c = 12;
-if (c++ === 13){
-    alert("condition 1 is true");   // Answer: alert message not displayed
-}
-if (c === 13){
-    alert("condition 2 is true");   // Answer: alert message displayed
-}
-if (++c < 14){
-    alert("condition 3 is true");   // Answer: alert message not displayed
-}
-if(c === 14){
-    alert("condition 4 is true");   // Answer: alert message displayed
-}
+// // c.
+// var c = 12;
+// if (c++ === 13){
+//     alert("condition 1 is true");   // Answer: alert message not displayed
+// }
+// if (c === 13){
+//     alert("condition 2 is true");   // Answer: alert message displayed
+// }
+// if (++c < 14){
+//     alert("condition 3 is true");   // Answer: alert message not displayed
+// }
+// if(c === 14){
+//     alert("condition 4 is true");   // Answer: alert message displayed
+// }
 
-// Answer: Condition 2 and 4 only displayed
+// // Answer: Condition 2 and 4 only displayed
 
-// d.
-var materialCost = 20000;
-var laborCost = 2000;
-var totalCost = materialCost + laborCost;
+// // d.
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
 
-if (totalCost === laborCost + materialCost){
-    alert("The cost equals");
-}
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals");
+// }
 
-// Answer: alert message displayed
+// // Answer: alert message displayed
 
-// e.
-if (true){
-    alert("True");      //Answer: alert message displayed
-}
-if (false){
-    alert("False");     //Answer: alert message not displayed
-}
+// // e.
+// if (true){
+//     alert("True");      //Answer: alert message displayed
+// }
+// if (false){
+//     alert("False");     //Answer: alert message not displayed
+// }
 
-// f.
-if("car" < "cat"){
-    alert("car is smaller than cat");
-}
+// // f.
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
 
-// Answer: alert message displayed
+// // Answer: alert message displayed
 
-// Q6,  Write a program to take input the marks obtained in three subjects & total marks. Compute & show
-// the resulting percentage on your page. Take percentage & compute grade as per following table:
-// Show the total marks, marks obtained, percentage, grade & remarks like:
+// // Q6,  Write a program to take input the marks obtained in three subjects & total marks. Compute & show
+// // the resulting percentage on your page. Take percentage & compute grade as per following table:
+// // Show the total marks, marks obtained, percentage, grade & remarks like:
 
-var obtMarks = Number(prompt("Enter a Obtained Marks in 3 Subjects"));
-var totalMarks = Number(prompt("Enter a Total Marks in 3 Subjects"));
+// var obtMarks = Number(prompt("Enter a Obtained Marks in 3 Subjects"));
+// var totalMarks = Number(prompt("Enter a Total Marks in 3 Subjects"));
 
-var percentage = (obtMarks / totalMarks) * 100;
+// var percentage = (obtMarks / totalMarks) * 100;
 
-document.write('<h1>Marksheet</h1>');
-document.write('<br><br> Total Marks: ', totalMarks);
-document.write('<br> Obtained Marks: ', obtMarks);
-document.write('<br> Percentage: ', percentage);
+// document.write('<h1>Marksheet</h1>');
+// document.write('<br><br> Total Marks: ', totalMarks);
+// document.write('<br> Obtained Marks: ', obtMarks);
+// document.write('<br> Percentage: ', percentage);
 
-if (percentage >= 80) {
-    document.write('<br><br> Grade: A-one');
-    document.write('<br><br> Remarks: Excellent');
-}
-else if (percentage >= 70) {
-    document.write('<br><br> Grade: A');
-    document.write('<br><br> Remarks: Good');
-}
-else if (percentage >= 60) {
-    document.write('<br><br> Grade: B');
-    document.write('<br><br> Remarks: You need to improve');
-}
-else if (percentage < 60) {
-    document.write('<br><br> Grade: Fail');
-    document.write('<br><br> Remarks: Sorry');
-}
+// if (percentage >= 80) {
+//     document.write('<br><br> Grade: A-one');
+//     document.write('<br><br> Remarks: Excellent');
+// }
+// else if (percentage >= 70) {
+//     document.write('<br><br> Grade: A');
+//     document.write('<br><br> Remarks: Good');
+// }
+// else if (percentage >= 60) {
+//     document.write('<br><br> Grade: B');
+//     document.write('<br><br> Remarks: You need to improve');
+// }
+// else if (percentage < 60) {
+//     document.write('<br><br> Grade: Fail');
+//     document.write('<br><br> Remarks: Sorry');
+// }
 
-// Q7, Guess game:
-// Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number.
-// a. If user guesses the same number, show “Bingo! Correct answer”.
-// b. If the guessed number +1 is the secret number, show “Close enough to the correct answer”.
+// // Q7, Guess game:
+// // Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number.
+// // a. If user guesses the same number, show “Bingo! Correct answer”.
+// // b. If the guessed number +1 is the secret number, show “Close enough to the correct answer”.
 
-var randNum = 5;
+// var randNum = 5;
 
-var guessNumber = Number(prompt("Guess a Number"));
+// var guessNumber = Number(prompt("Guess a Number"));
 
-if(guessNumber == randNum){
-    console.log("Bingo! Correct answer");
+// if(guessNumber == randNum){
+//     console.log("Bingo! Correct answer");
+// }
+// else {
+//     console.log('Close enough to the correct answer');
+// }
+
+// // Q8, Write a program to check whether the given number is divisible by 3. Show the message to the
+// // user if the number is divisible by 3.
+
+// var divByThree = Number(prompt('Enter a Number'));
+
+// if (divByThree % 3 == 0) {
+//   console.log('A number is divided by three', divByThree);
+// }
+// else{
+//   console.log(`A number ${divByThree} is divided by three`);
+// }
+
+// // Q9, Write a program that checks whether the given input is an even number or an odd number.
+
+// var checkInput = +prompt("Enter any number");
+
+// if (checkInput % 2 == 0) {
+//   console.log('This is even number', checkInput);
+// }
+// else {
+//   console.log('This is Odd number', checkInput);
+// }
+
+// Q10, Write a program that takes temperature as input and shows a message based on following criteria.
+// a.T > 40 then “It is too hot outside.”
+// b. T > 30 then “The Weather today is Normal.”
+// c. T > 20 then “Today’s Weather is cool.”
+// d. T > 10 then “OMG! Today’s weather is so Cool.”
+
+// var temperature = +prompt('Enter a temperature');
+
+// if (temperature > 40) {
+//   console.log('It is too hot outside.');
+// }
+// else if (temperature > 30) {
+//   console.log('The Weather today is Normal.');
+// }
+// else if (temperature > 20) {
+//   console.log('Today’s Weather is cool.');
+// }
+// else if (temperature > 10) {
+//   console.log('OMG! Today’s weather is so Cool.');
+// }
+
+// Q11, Write a program to create a calculator for +,-,*, / & % using if statements. Take the following input:
+// a. First number
+// b. Second number
+// c. Operation (+, -, *, /, %) Compute & show the calculated result to user.
+
+var num1 = +prompt("Enter a First Number");
+var num2 = +prompt("Enter a Second Number");
+var operation = prompt("Enter an operation (+, -, *, /, %)");
+
+if (operation != "") {
+  if (operation == "+") {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+  }
+  else if (operation == "-") {
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+  }
+  else if (operation == "*") {
+    console.log(`${num1} * ${num2} = ${num1 * num2}`);
+  }
+  else if (operation == "/") {
+  if (num2 !== 0) {
+      console.log(`${num1} / ${num2} = ${num1 / num2}`);
+  }
+  else {
+      console.log("Division by zero is not allowed");
+    }
+  }
+  else if (operation == "%") {
+    console.log(`${num1} % ${num2} = ${num1 % num2}`);
+  }
+  else {
+    console.log("Invalid operation");
+  }
 }
 else {
-    console.log('Close enough to the correct answer');    
+  console.log("Please enter a valid operation");
 }
-
-// Q8, Write a program to check whether the given number is divisible by 3. Show the message to the 
-// user if the number is divisible by 3.
-
-var divByThree = Number(prompt('Enter a Number'));
-
-console.log('A number is divided by three', );
--- JavaScript/Tasks/main.js
+++ JavaScript/Tasks/main.js
@@ -155,27 +155,110 @@
 
 // how to find minimum and maximum values in array without using built-in function?
 
-var arr = [20,3,33,40, 20, 300, 800, 10, 440, 900, 30, 200, 40];
-var max = 0;
-var secondMax = 0;
-var min = arr[0];
-var secondMin = arr[0];
+// var arr = [20,3,33,40, 20, 300, 800, 10, 440, 900, 30, 200, 40];
+// var max = 0;
+// var secondMax = 0;
+// var min = arr[0];
+// var secondMin = arr[0];
 
-for (let i = 0; i < arr.length; i++) {
-    if (max < arr[i]) {
-        max = arr[i];
-        if(max < arr[i] && secondMax < arr[i]){
-            secondMax = arr[i];
-        }
-    }    
+// for (let i = 0; i < arr.length; i++) {
+//     if (max < arr[i]) {
+//         max = arr[i];
+//         if(max < arr[i] && secondMax < arr[i]){
+//             secondMax = arr[i];
+//         }
+//     }    
     
-    if (min > arr[i]) {
-        min = arr[i];
+//     if (min > arr[i]) {
+//         min = arr[i];
+//     }
+// }
+
+// console.log('Maxiumum Value in Array:', max);
+// console.log('Second Maxiumum Value in Array:', secondMax);
+
+// console.log('Minimum Value in Array:', min);
+// console.log('Second Minimum Value in Array:', secondMin);
+
+
+
+
+
+//random array ko sort krna hai ascending and descending order me without bui-in function
+var randomNum = [300, 20, 34, 50, 100, 700, 400, 350, 10];
+var temp;
+
+for (let i = 0; i < randomNum.length; i++) {
+    for (let j = 0; j < i; j++) {
+        if (i < j) {
+            temp = i;
+            j=i;
+            i=temp;
+        }
     }
 }
 
-console.log('Maxiumum Value in Array:', max);
-console.log('Second Maxiumum Value in Array:', secondMax);
+console.log(temp);
 
-console.log('Minimum Value in Array:', min);
-console.log('Second Minimum Value in Array:', secondMin);
+
+
+// Task 2, 5 subject ka nmae lena hai or unka numbers lena hai or har subject ki individual percntage nklni hai or 
+
+// var subjectCount = Number(prompt("Enter a number of subject you want to make marksheet"));
+// var subName = [];
+// var subObtainedMarks = [];
+// var totalMrks = 100;
+// var percntage = [];
+// var totalPercentage = 0;
+
+// for (let i = 1; i <= subjectCount; i++) {
+//     var getName = prompt(`Enter a subject ${i} name`);
+//     subName.push(getName);
+
+//     for (let j = i; j <= i; j++) {
+//         var ObtainedMarks = Number(prompt(`Enter a marks of subject ${j}`));
+//         subObtainedMarks.push(ObtainedMarks);
+//     }
+// }
+
+// for (let i = 0; i < subObtainedMarks.length; i++) {
+//     var getPercentage = (subObtainedMarks[i] / totalMrks) * 100; 
+//     percntage.push(getPercentage);
+// }
+
+// for (let i = 0; i < percntage.length; i++) {
+//     totalPercentage += percntage[i];
+// }
+
+// var finalPercntage = (totalPercentage / (totalMrks * subjectCount)) * 100;
+
+// document.write('<h1>Marksheet<br><br></h1>');
+// document.write('Subject Count: ', subjectCount);
+// document.write('<br>Total Marks<br>: ', totalMrks);
+
+// for (let i = 0; i < subjectCount; i++) {
+//     document.write(`<br>${i+1} subject name is ${subName[i]} <br> `);
+//     document.write(`${i+1} subject obtained marks is ${subObtainedMarks[i]} <br>`);
+//     document.write(`${i+1} subject percenage is ${percntage[i]} % <br>`);
+// }
+// document.write('<br>Final Percentage is: ', finalPercntage + "%");
+
+// Task 3 input from user and show pattern
+
+// var rowCount = +prompt('Enter a count for pattern');
+
+// for (let i = 1; i <= rowCount; i++) {
+//     let pattern = '';
+//     for (let j = 1; j <= i; j++) {
+//         pattern += '*'
+//     }
+//     console.log(pattern);
+// }
+
+// for (let i = rowCount; i > 0; i--) {
+//     let pattern = '';
+//     for (let j = 0; j < i; j++) {
+//         pattern += '*'
+//     }
+//     console.log(pattern);
+// }
